8ec5cbfb3ef8c47b98e47a41cf91bc42ece8b0b5
MT <42926446+MTxWISDOM@users.noreply.github.com>
2019-03-12 21:41:10 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,50 +1,40 @@
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-template<class E>
-struct Node
+struct apple
 {
-    E data;
-    Node<E> * next;
-
-    Node();
-    Node(E item, Node<E> *link = NULL);
+    int appleHeight;
+    int applePower;
 };
 
-template<class E>
-Node<E>::Node()
+bool cmp(apple a1, apple a2)
 {
-    next = NULL;
+    return a1.applePower < a2.applePower;
 }
-
-template<class E>
-Node<E>::Node(E item, Node<E> *link)
-{
-    data = item;
-    next = link;
-}
-
-template<class E>
-class List
-{
-protected:
-    Node<E> *head;
-    Node<E> *GetPtr(int pos) const;
-public:
-    List();
-    ~List();
-    int Length() const;
-    bool Empty() const;
-    void Clear();
-    void Traverse(void(*visit)(const E &)) const;
-    bool GetElem(int position, E &e) const;
-    bool SetElem(int positionm, const E &e);
-    bool Delete(int positionm, E &e);
-    bool Insert(int position, const E &e);
-
-};
 int main()
 {
-
+    apple A[5001];
+    int n, s;
+    int chairHeight, handHeight;
+    int cau = 0;
+    cin >> n >> s >> chairHeight >> handHeight;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i].appleHeight >> A[i].applePower;
+    }
+    sort(A, A + n, cmp);
+    int index = 0;
+    while(s >= 0 && index < n)
+    {
+        if(A[index].appleHeight <= chairHeight + handHeight && A[index].applePower <= s)
+        {
+            cau++;
+            s -= A[index].applePower;
+        }
+        index++;
+    }
+    cout << cau <<endl;
+    return 0;
 }
